eda13d7a8713b6749602680db9948fe4a8b748e7
Kalutu Daniel <kalutudaniel@gmail.com>
2023-04-15 06:24:24 +0300

    Update guessgame.cpp

--- Guessing Game/guessgame.cpp
+++ Guessing Game/guessgame.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -14,7 +16,26 @@ bool compareScores(const Score& a, const Score& b) {
     return a.score > b.score;
 }
 
+void displayHighScores(const vector<Score>& scores) {
+    cout << "High Scores:" << endl;
+    for (int i = 0; i < scores.size(); i++) {
+        cout << scores[i].name << ": " << scores[i].score << endl;
+    }
+}
+
+void writeHighScores(const vector<Score>& scores, const string& filename) {
+    ofstream outfile(filename);
+    if (outfile.is_open()) {
+        for (int i = 0; i < scores.size(); i++) {
+            outfile << scores[i].name << " " << scores[i].score << endl;
+        }
+        outfile.close();
+    }
+}
+
 int main() {
+    srand(time(NULL));  // Initialize random number generator
+
     vector<Score> scores;
 
     ifstream infile("highscores.txt");
@@ -28,35 +49,35 @@ int main() {
         infile.close();
     }
 
-    cout << "High Scores:" << endl;
-    for (int i = 0; i < scores.size(); i++) {
-        cout << scores[i].name << ": " << scores[i].score << endl;
-    }
+    displayHighScores(scores);
 
-    string name;
-    int score;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Enter your score: ";
-    cin >> score;
+    int randomNumber = rand() % 100 + 1;  // Generate a random number between 1 and 100
+    int guess;
+    int attempts = 0;
 
-    Score s = { name, score };
-    scores.push_back(s);
+    cout << "Welcome to the guessing game! Try to guess the number between 1 and 100." << endl;
 
-    sort(scores.begin(), scores.end(), compareScores);
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
 
-    ofstream outfile("highscores.txt");
-    if (outfile.is_open()) {
-        for (int i = 0; i < scores.size(); i++) {
-            outfile << scores[i].name << " " << scores[i].score << endl;
+        if (guess < randomNumber) {
+            cout << "Too low. Try again." << endl;
+        } else if (guess > randomNumber) {
+            cout << "Too high. Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
+            string name;
+            cout << "Enter your name for the high scores list: ";
+            cin >> name;
+            Score s = { name, attempts };
+            scores.push_back(s);
+            sort(scores.begin(), scores.end(), compareScores);
+            writeHighScores(scores, "highscores.txt");
+            displayHighScores(scores);
         }
-        outfile.close();
-    }
-
-    cout << "High Scores:" << endl;
-    for (int i = 0; i < scores.size(); i++) {
-        cout << scores[i].name << ": " << scores[i].score << endl;
-    }
+    } while (guess != randomNumber);
 
     return 0;
 }
